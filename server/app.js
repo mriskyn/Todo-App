9831febbaf460c44a605e18029c180e9aba9dd75
Muhammad Risky Nugraha <muhammadrisky83@gmail.com>
2020-11-21 19:40:51 +0700

    setup app.js

--- server/app.js
+++ server/app.js
@@ -1,3 +1,12 @@
-require('dotenv').config()
-const express = require('express');
-const cors = require('cors')
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const PORT = process.env.PORT || 3300;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(PORT, () => console.log("Server Hosted on:", PORT));
